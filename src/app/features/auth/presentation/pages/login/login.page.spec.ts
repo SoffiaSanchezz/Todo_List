3b4c207b448b5d02ee4fd69210b40bf9338dfce1
SoffiaSanchezz <ssofiiadayanna@gmail.com>
2026-03-02 07:30:02 -0500

    test(auth): agregar prueba básica de creación para LoginPage con mocks de dependencias

--- src/app/features/auth/presentation/pages/login/login.page.spec.ts
+++ src/app/features/auth/presentation/pages/login/login.page.spec.ts
@@ -1,16 +1,26 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
-
 import { LoginPage } from './login.page';
+import { IonicModule, ModalController } from '@ionic/angular';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SessionProviderservice } from '@shared/services/auth/session-provider.service';
+import { Router } from '@angular/router';
 
 describe('LoginPage', () => {
   let component: LoginPage;
   let fixture: ComponentFixture<LoginPage>;
 
   beforeEach(waitForAsync(() => {
+    const mockAuth = jasmine.createSpyObj('SessionProviderservice', ['login', 'signInWithGoogle']);
+    const mockRouter = jasmine.createSpyObj('Router', ['navigateByUrl']);
+    const mockModal = jasmine.createSpyObj('ModalController', ['create']);
+
     TestBed.configureTestingModule({
-      declarations: [ LoginPage ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot(), ReactiveFormsModule, LoginPage],
+      providers: [
+        { provide: SessionProviderservice, useValue: mockAuth },
+        { provide: Router, useValue: mockRouter },
+        { provide: ModalController, useValue: mockModal }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginPage);
